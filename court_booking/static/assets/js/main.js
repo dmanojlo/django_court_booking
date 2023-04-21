1c3766ba8f21b164130197b056e0cf04b0d76ac5
dmanojlo <dragojlo@net.hr>
2023-04-21 16:53:58 +0200

    Ini

--- court_booking/static/assets/js/main.js
+++ court_booking/static/assets/js/main.js
@@ -4,43 +4,13 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function($) {
+$(document).ready(function () {
+
+$(function() {
 
 	var	$window = $(window),
-		$body = $('body'),
-		$header = $('#header'),
-		$banner = $('#banner');
-
-	// Breakpoints.
-		breakpoints({
-			xlarge:	'(max-width: 1680px)',
-			large:	'(max-width: 1280px)',
-			medium:	'(max-width: 980px)',
-			small:	'(max-width: 736px)',
-			xsmall:	'(max-width: 480px)'
-		});
-
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
-
-	// Header.
-		if ($banner.length > 0
-		&&	$header.hasClass('alt')) {
-
-			$window.on('resize', function() { $window.trigger('scroll'); });
-
-			$banner.scrollex({
-				bottom:		$header.outerHeight(),
-				terminate:	function() { $header.removeClass('alt'); },
-				enter:		function() { $header.addClass('alt'); },
-				leave:		function() { $header.removeClass('alt'); }
-			});
-
-		}
+		$body = $('body')
+		
 
 	// Menu.
 		var $menu = $('#menu');
@@ -142,4 +112,5 @@
 
 			});
 
-})(jQuery);
+})
+});
